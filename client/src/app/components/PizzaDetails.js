1c92fc8c9a8ea82cdc6c1e5bcee149d153973d42
Pavel Belousov <signorpao@gmail.com>
2023-07-31 11:28:23 +0200

    ...modal...

--- client/src/app/components/PizzaDetails.js
+++ client/src/app/components/PizzaDetails.js
@@ -35,9 +35,80 @@ const PizzaDetails = ({ pizza }) => {
       : null;
   });
 
-  console.log(price);
+  // pizza price with topping
+  useEffect(() => {
+    if (topping.length > 0) {
+      const toppingPrice = topping.reduce((a, c) => {
+        return a + c.price;
+      }, 0);
+      setToppingPrice(toppingPrice);
+    } else {
+      setToppingPrice(0);
+    }
+  }, [topping]);
 
-  return <div>PizzaDetails</div>;
+  return (
+    <div className="flex flex-col lg:flex-row lg:gap-x-8 h-full md:p-8">
+      {/* top */}
+      <div className="lg:flex-1 flex justify-center items-center">
+        {/* pizza img */}
+        <div className="max-w-[300px] lg:max-w-none mt-6 lg:mt-0">
+          <Image
+            width={450}
+            height={450}
+            src={pizza.image}
+            alt="pizza"
+            priority={true}
+            className="mx-auto relative"
+          />
+        </div>
+      </div>
+
+      {/* details */}
+      <div className="bg-green-300 flex flex-col flex-1">
+        <div className="flex-1 p-2 text-center lg:text-left">
+          <div className="flex-1 bg-white overflow-y-scroll h-[40vh] scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-white pr-2">
+            {/* name */}
+            <div className="font-semibold">
+              <h2 className="capitalize text-xl lg:text-3xl mb-1">
+                {pizza.name}
+              </h2>
+              <div className="bg-yellow-300 mb-6 text-lg font-medium">
+                <span>
+                  {size === "small"
+                    ? "25 cm"
+                    : size === "medium"
+                    ? "30 cm"
+                    : size === "large"
+                    ? "35cm"
+                    : null}
+                </span>
+                <span>, {crust} crust</span>
+              </div>
+            </div>
+            {/* size */}
+            <SizeSelection />
+            {/* crust */}
+            <CrustSelection />
+            {/* topping */}
+            <div>Choose topping</div>
+            <div>
+              {pizza.toppings.map((topping, index) => {
+                return <Topping key={index} />;
+              })}
+            </div>
+          </div>
+        </div>
+        {/* add to cart btn */}
+        <div className="h-full flex items-center px-2 lg:items-end">
+          <button className="btn btn-lg gradient w-full flex justify-center gap-x-2">
+            <div>Add to cart for</div>
+            <div>$ {price}</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PizzaDetails;
